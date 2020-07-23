e769c32d4aab51154306a4b5d2d292285a23870a
Cristhian Torres <cristhiantorres95@gmail.com>
2020-07-23 15:17:18 -0400

    scraping tuquinelateete.com

--- index.js
+++ index.js
@@ -3,39 +3,67 @@ const rp = require('request-promise');
 const $ = require('cheerio');
 const potusParse = require('./potusParse');
 
-const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
-const wikiUrl = 'https://en.wikipedia.org';
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+const url = 'https://www.tuquinielateete.com/Resultados';
+const detailUrl = 'https://www.tuquinielateete.com';
 
 rp(url)
 .then(function (html) {
-  let data = $('b > a', '.wikitable', html);
-  let wikiLinks = [];
-  
+  let data = $('a', 'table', html);
+  let links = [];
+
   // Message
   console.log('starting web scraping...');
 
-  data.each(function(index, element){
+  data.map((index, element) => {
     let href = element.attribs.href; 
-
     if (href) {
-      wikiLinks.push(href);
+      links.push(href);
     }
   });
 
+  console.log('link count', links.length);
   return Promise.all(
-    wikiLinks.map((link) => {
-      return potusParse(wikiUrl + link);
+    
+    links.map((link) => {
+      let tmp
+      setInterval(() => {
+        console.log('potus...');
+        tmp = potusParse(detailUrl + link);
+      }, 5000);
+
+      return tmp;
     })
   )
 })
-.then((presidents) => {
+.then((items) => {
   
-  console.log(presidents);
-  
+  let fileName = items[0];
+  console.log(fileName);
+  // createCsv(items, fileName)
+
   // Message
   console.log('end web scraping...');
 })
 .catch(function (error) {
-  console.error(error);
+  console.error("Error Index", error);
 });
 
+
+const createCsv = (data, fileName) => {
+  const csvWriter = createCsvWriter({
+    path: `assets/${fileName}.csv`,
+    header: [
+      { id: 'posicion', title: 'posicion' },
+      { id: 'fecha', title: 'fecha' },
+      { id: 'horario', title: 'horario' },
+      { id: 'numero', title: 'numero' },
+    ]
+  });
+
+  csvWriter
+    .writeRecords(data)
+    .then(() => console.log('CSV created'));
+
+}
--- potusParse.js
+++ potusParse.js
@@ -1,18 +1,112 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
-
-const regex = /[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]/gm;
+const months = [
+  { 'key': 'enero', 'value': '01' },
+  { 'key': 'febrero', 'value': '02' },
+  { 'key': 'marzo', 'value': '03' },
+  { 'key': 'abril', 'value': '04' },
+  { 'key': 'mayo', 'value': '05' },
+  { 'key': 'junio', 'value': '06' },
+  { 'key': 'julio', 'value': '07' },
+  { 'key': 'agosto', 'value': '08' },
+  { 'key': 'septiembre', 'value': '09' },
+  { 'key': 'octubre', 'value': '10' },
+  { 'key': 'noviembre', 'value': '11' },
+  { 'key': 'diciembre', 'value': '12' },
+];
 
 const potusParse = (url) => {
   return rp(url)
     .then((html) => {
-      let name = $('#firstHeading', html).text();
-      let bday = $('.bday', html).text();
-      return { name: name, birthday: bday };
+      
+      let tables = $('.detalle-resultado', html)
+      
+      let dates = [];
+      let titles = [];
+      let rows = [];
+      let posi = [];
+
+      tables.map((index, element) => {
+        
+        let date = getDate($('.fecha', element).first().text().trim());
+        dates.push(date);
+        
+        let title = $('.cifra-titulo', '.row.cabecera', element);
+        let tmpTitle = [];
+        title.map((index, t) => {
+          tmpTitle.push($(t).text().trim())
+        });
+        titles.push(tmpTitle)
+
+        let pos = $('.postura', '.row', element);
+        let tmpPos = [];
+        
+        pos.map((index, p) => {
+          if ($(p).text().trim()) {
+            tmpPos.push($(p).text().trim());
+          }
+        });
+
+        posi.push(tmpPos);
+
+        let row = $('.cifra', '.row', element);
+        let tmpRows = [];
+        row.map((index, r) => {
+          tmpRows.push($(r).text().trim());
+        });
+
+        rows.push(tmpRows);
+
+      });
+
+      let data = [];
+      let positions = 13;
+
+      for (let a = 0; a < dates.length; a++) {
+        
+        let titlesCount = titles[a].length - 1 ;
+        let count = titles[a].length - 1;
+      
+        for (let b = 0; b < rows[a].length; b++) {
+          let index = count - titlesCount;
+
+          let item = {
+            'posicion': 14 - positions,
+            'fecha': dates[a],
+            'horario': titles[a][index],
+            'numero': rows[a][b],
+          };
+
+          data.push(item);
+          
+          if (titlesCount == 0) {
+            titlesCount = count;
+            positions--;
+          } else {
+            titlesCount--;
+          }
+        }
+        positions = 13;
+      }
+
+      return data;
     })
     .catch((error) => {
-      return {};
+      console.log('url', url);
+      console.error('Error potus', error);
     }); 
+}
+
+const getDate = (date) => {
+  let arrayDate = date.split(" ");
+
+  let day = arrayDate[1];
+  let month = months.find((item) => {
+    return item.key == arrayDate[2];
+  }).value;
+  let year = arrayDate[3];
+
+  return `${day}-${month}-${year}`;
 }
 
 module.exports = potusParse; 
